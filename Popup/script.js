37e27691ae301d7529d48fa6591c47e16210e827
Pritesh Mishra <90691640+impriteshmishra@users.noreply.github.com>
2024-02-03 23:59:41 +0530

    Add files via upload

---
+++ Popup/script.js
@@ -0,0 +1,14 @@
+let  popup = document.getElementById("popup");
+// let Popup = document.querySelector(".Popup")
+// let open=document.getElementById("open");
+// let close = document.getElementById("close");
+function openPopup(){
+    popup.classList.add("open-Popup");
+    // Popup.classList.add("open-Popup");
+    // Popup.classList.add('open-Popup');
+}
+function closePopup(){
+    popup.classList.remove("open-Popup");
+    // Popup.classList.remove("open-Popup");
+}
+
